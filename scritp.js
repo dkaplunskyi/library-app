a721669ac5ce0caca8695e20b2a7022e4ed0bfad
Dmytro Kaplunskyi <dkaplunsky@gmail.com>
2024-07-04 20:56:50 -0400

    add style to the dialog window

--- scritp.js
+++ scritp.js
@@ -4,28 +4,70 @@ const pages = document.querySelector('#pages');
 const isFinished = document.querySelector('#isFinished');
 const output = document.querySelector('.output');
 const submitBtn = document.querySelector('#submit');
+const closeDialogBtn = document.querySelector('.close-btn');
+const dialog = document.querySelector('#dialog');
+
+const addBook = document.querySelector('#addBook');
 
 const myLibrary = [];
 
-function Book(author, title, pages, isFinished) {
-  this.author = author;
+function Book(title, author, pages, isFinished) {
   this.title = title;
+  this.author = author;
   this.pages = pages;
   this.isFinished = isFinished;
 }
 
-function addBookToLibrary() {
-  myLibrary.push(new Book(author.value, title.value, pages.value, isFinished.checked));
+const book1 = new Book('Throne of Glass', 'Sarah J. Maas', 321, false);
+const book2 = new Book('The Godfather', '	Mario Puzo', 777, false);
+const book3 = new Book('The Godfather 2', '	Mario Puzo', 876, false);
+myLibrary.push(book1);
+myLibrary.push(book2);
+myLibrary.push(book3);
 
-  for (const book of myLibrary) {
-    for (const key in book) {
-      output.textContent += `${key}: ${book[key]}; `;
-    }
+
+function addBookToLibrary() {
+  // clear output
+  while (output.firstChild) {
+    output.removeChild(output.firstChild);
   }
 
+  // add new book to the book library array
+  myLibrary.push(new Book(title.value, author.value, pages.value, isFinished.checked));
+
+  // display books item
+  display(myLibrary);
 }
+
+// create a card for each book item
+// and display them in the output
+function display(array) {
+  array.forEach(book => {
+    const card = document.createElement('div');
+    const ul = document.createElement('ul')
+    for (const key in book) {
+      const li = document.createElement('li');
+      li.textContent = `${key.charAt(0).toUpperCase() + key.slice(1)} - ${book[key]}`;
+      ul.appendChild(li);
+    }
+    card.appendChild(ul);
+    card.classList.add('card-item')
+    output.appendChild(card);
+  });
+}
+
+display(myLibrary);
+
+addBook.addEventListener('click', () => {
+  dialog.showModal();
+})
+
+closeDialogBtn.addEventListener('click', () => {
+  dialog.close();
+})
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
   addBookToLibrary();
+  dialog.close();
 });
